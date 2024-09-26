29ebe96eeb2701501942b9e2ddc148e82660e893
Christian Fryksten <christianfryksten1@gmail.com>
2024-09-26 11:52:16 +0200

    Including more files and added some more tests

---
+++ toUpperCase.js
@@ -0,0 +1,7 @@
+function toUpper(text) {
+  const upperText = text.toUpperCase();
+  console.log(upperText);
+  return upperText;
+}
+
+module.exports = toUpper;
--- toUpperCase.test.js
+++ toUpperCase.test.js
@@ -1,5 +1,11 @@
-const toUpper = require('./toUpperCase')
+const toUpper = require("./toUpperCase");
 
-test('changes text to upper case', () => {
-  expect(toUpper("hello there")).toBe("HELLO THERE")
+test("changes text to upper case", () => {
+  expect(toUpper("hello there")).toBe("HELLO THERE");
+  expect(toUpper("I should have BIG letters")).toBe(
+    "I SHOULD HAVE BIG LETTERS"
+  );
+  expect(toUpper("Just a third test so we know for suuuuuuure")).toBe(
+    "JUST A THIRD TEST SO WE KNOW FOR SUUUUUUURE"
+  );
 });
